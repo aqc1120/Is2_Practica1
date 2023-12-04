2a1fd751e690cb3dccdefe0140694e6ddf3ba9aa
ignatejero <103136590+ignatejero@users.noreply.github.com>
2023-12-04 20:27:20 +0100

    Modificación archivo forms.py y página opinion.html añadidad al navbar en el archivo base.html

--- relecloud/forms.py
+++ relecloud/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from .models import Cruise
 
 class OpinionForm(forms.Form):
-    name = forms.CharField(label='Nombre', max_length=100)
-    cruise = forms.ModelChoiceField(queryset=Cruise.objects.all(), label='Crucero')
-    opinion = forms.TextField(label='Opinión')
+    name = forms.CharField(label='Nombre', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cruise = forms.ModelChoiceField(queryset=Cruise.objects.all(), label='Crucero', widget=forms.Select(attrs={'class': 'form-control'}))
+    opinion = forms.CharField(label='Opinión', widget=forms.Textarea(attrs={'rows': 5, 'cols': 40, 'class': 'form-control'}))
