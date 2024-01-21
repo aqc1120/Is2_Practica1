4819a365b94801ba4cc4833da3fdd858dd31083f
Ignacio Ruiz-Oriol <iruizoriol@enaque.es>
2024-01-21 18:05:22 +0100

    Update settings to deploy

--- proyect/settings.py
+++ proyect/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xxj^d3s_r-t(#!d$&&qi&hl4k)2r$0g(41buem(413+wp7l=rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #Cambiar el path del host al de deploy
-#ALLOWED_HOSTS = ['tejr-space-tours.azurewebsites.net']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tejr-space-tours.azurewebsites.net']
+
 
 
 
